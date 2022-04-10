d04245487955de6ba45cdf7e32ec9b36f318c595
yuvraj <yuvraj.seegolam@umail.uom.ac.mu>
2022-04-10 12:07:38 +0400

    create image sharp

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("Hello from app.js");
+const sharp = require("sharp");
+
+async function addTextOnImage() {
+  try {
+    const width = 750;
+    const height = 483;
+    const text = "Sammy the Shark";
+
+    const svgImage = `
+    <svg width="${width}" height="${height}">
+      <style>
+      .title { fill: #001; font-size: 70px; font-weight: bold;}
+      </style>
+      <text x="50%" y="50%" text-anchor="middle" class="title">${text}</text>
+    </svg>
+    `;
+    const svgBuffer = Buffer.from(svgImage);
+    const image = await sharp(svgBuffer).toFile("outputs/svg-image.png");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+addTextOnImage();
